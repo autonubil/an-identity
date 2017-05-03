000aad22aa86eb987fd74c917ea7ca29a5a14f55
ruben malchow <ruben.malchow@gmail.com>
2017-05-03 16:31:02 +0200

    [ci-skip] release prepare 0.1.4

--- an-identity-auth/an-identity-auth-client/src/main/java/com/autonubil/identity/auth/api/client/RestAuthClientFactory.java
+++ an-identity-auth/an-identity-auth-client/src/main/java/com/autonubil/identity/auth/api/client/RestAuthClientFactory.java
@@ -3,20 +3,19 @@ package com.autonubil.identity.auth.api.client;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 
 public class RestAuthClientFactory {
 
 	private static final Log log = LogFactory.getLog(RestAuthClientFactory.class);
 	
-	
-	@ConditionalOnProperty(matchIfMissing=false,name="an.identity.server.host")
 	public RestAuthClient restAuthClient(
-				@Value("an.identity.server.host") String host,
-				@Value("an.identity.server.port") int port
+				@Value("an.identity.server.url") String url
 			) {
-		
-		log.info("instantiating rest client .... ");
+		if(url=="") {
+			log.info("NOT instantiating rest client for url: "+url);
+			return null;
+		}
+		log.info("instantiating rest client for url: "+url);
 		return new RestAuthClient();
 	}
 	
