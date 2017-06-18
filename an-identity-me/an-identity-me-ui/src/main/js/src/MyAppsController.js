5429a68c760d8ae95cf1ecb8bbfb77d1b92331fa
ruben malchow <ruben.malchow@gmail.com>
2017-06-18 13:31:37 +0200

    fix app list filter

--- an-identity-me/an-identity-me-ui/src/main/js/src/MyAppsController.js
+++ an-identity-me/an-identity-me-ui/src/main/js/src/MyAppsController.js
@@ -1,13 +1,18 @@
 angular.module("autonubil-intranet-me")
 .controller("MyAppsController", function($scope,MeService,AuthService,$location) {
 	
-	$scope.search =  "";
+	$scope.search = {
+			term : ""
+	};
+	
+	console.log("search initialized to: ",$scope.search);
 
-	$scope.updateApps = _.debounce(function() {
-		MeService.getApps({search:$scope.search},function(apps){
+	$scope.updateApps = function() {
+		console.log("updating apps with filter: "+$scope.search.term);
+		MeService.getApps({search:$scope.search.term},function(apps){
 			$scope.apps = apps;
 		});
-	},250);
+	};
 	
 	$scope.updateApps();
 	
